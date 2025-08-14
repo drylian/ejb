e85787854e0f0b9713b7f114060ef2e7da5fe6a6
Drylian <109999325+drylian@users.noreply.github.com>
2025-08-14 12:10:16 -0400

    Updated constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import type { EjbDirectivePlugin } from "./types";
 export const EJB_DEFAULT_PREFIX_GLOBAL = 'it';
 export const EJB_DEFAULT_PREFIX_DIRECTIVE = '@';
-export const EJB_DEFAULT_PREFIX_VARIABLE = '{*}';
+export const EJB_DEFAULT_PREFIX_VARIABLE = '{{*}}';
 export const ESCAPE_HTML = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;', };
 export const ESPACE_HTML_REGEX = /[&<>"']/g;
 export const EJB_HEAD_DIRECTIVE_REPLACER = '<!--$EJB-HEAD-REPLACER-->'
