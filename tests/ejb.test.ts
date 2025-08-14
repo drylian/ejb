0b9028b19c5bc8c24656ad07d20efd71d7060ff5
Drylian <109999325+drylian@users.noreply.github.com>
2025-08-14 11:19:19 -0400

    Updated ejb.test.ts

--- tests/ejb.test.ts
+++ tests/ejb.test.ts
@@ -5,19 +5,5 @@ const ejb = new Ejb({
     async:true,
     resolver:EJBNodeJSResolver('./tests/views', true)
 });
-
-test("EJB Test View with @css and @head directives", async () => {
-    const result = await ejb.render('./ejb.test.ejb');
-
-    // Assertions
-    expect(result).toContain('<meta charset="utf-8">');
-    expect(result).toContain('<title>EJB Test View</title>');
-    expect(result).toContain(`body {
-            font-family: sans-serif;
-            background-color: #f0f0f0;
-        }`);
-    expect(result).toContain(`h1 {
-            color: navy;
-        }`);
-    expect(result).not.toContain('<!--$ejb-head-replace-->');
-});
+const result = await ejb.render('./ejb.test.ejb');
+console.log(result)
